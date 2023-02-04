081e667dbd243b1b9145cd07d4dfd59f73156ea0
Andrii Chernikov <2834980C@student.gla.ac.uk>
2023-02-04 15:20:56 +0000

    Chapter 6 and 7

--- rango/models.py
+++ rango/models.py
@@ -1,12 +1,19 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=128, unique=True)
+    NAME_MAX_LENGTH = 128
+    name = models.CharField(max_length=NAME_MAX_LENGTH, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -17,7 +24,9 @@ class Category(models.Model):
 
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    title = models.CharField(max_length=128)
+    TITLE_MAX_LENGTH = 128
+    title = models.CharField(max_length=TITLE_MAX_LENGTH)
+    URL_MAX_LENGTH = 200
     url = models.URLField()
     views = models.IntegerField(default=0)
 
